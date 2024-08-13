bd09b1f8855f715f04838e6cffc803a083e1d3db
Shaanjot Gill <gill.shaanjots@gmail.com>
2024-08-12 18:14:59 -0700

    fix key typo
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-api/tests/unit/services/test_approval.py
+++ strr-api/tests/unit/services/test_approval.py
@@ -138,7 +138,7 @@ def test_process_auto_approval(
             country="Canada",
         )
         expected_status = Application.Status.UNDER_REVIEW
-    token_dict = {"token": sample_token, "token_dict": {}}
+    token_dict = {"token": sample_token, "token_info": {}}
     approval_record = ApprovalService.process_auto_approval(token_dict=token_dict, application=application)
 
     assert approval_record is not None
