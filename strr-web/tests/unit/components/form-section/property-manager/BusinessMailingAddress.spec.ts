8d63757313b39f64ebfd4bb1db7e9e9540c4b44c
Shaanjot Gill <gill.shaanjots@gmail.com>
2024-10-25 16:49:27 -0700

    update pm unit test to include findTestId
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-web/tests/unit/components/form-section/property-manager/BusinessMailingAddress.spec.ts
+++ strr-web/tests/unit/components/form-section/property-manager/BusinessMailingAddress.spec.ts
@@ -7,9 +7,10 @@ import { BcrosFormSectionPropertyManagerBusinessMailingAddress } from '#componen
 const i18n = createI18n({
   // vue-i18n options here ...
 })
+let wrapper: any
 
 it('can mount Business Mailing Address component', async () => {
-  const businessMailingAddress = await mountSuspended(BcrosFormSectionPropertyManagerBusinessMailingAddress,
+  wrapper = await mountSuspended(BcrosFormSectionPropertyManagerBusinessMailingAddress,
     {
       global: { plugins: [i18n] },
       props: {
@@ -19,16 +20,16 @@ it('can mount Business Mailing Address component', async () => {
         errors: {}
       }
     })
-  expect(businessMailingAddress.find('[data-test-id="property-manager-business-address"]').exists()).toBe(true)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-country-select"]').exists()).toBe(true)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-address-input"]').exists()).toBe(true)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-address-line-two-input"]').exists()).toBe(true)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-city-input"]').exists()).toBe(true)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-postal-code-input"]').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-business-address').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-country-select').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-address-input').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-address-line-two-input').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-city-input').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-postal-code-input').exists()).toBe(true)
 })
 
 it('shows province select for CA and US', async () => {
-  const businessMailingAddress = await mountSuspended(BcrosFormSectionPropertyManagerBusinessMailingAddress,
+  wrapper = await mountSuspended(BcrosFormSectionPropertyManagerBusinessMailingAddress,
     {
       global: { plugins: [i18n] },
       props: {
@@ -38,12 +39,12 @@ it('shows province select for CA and US', async () => {
         errors: {}
       }
     })
-  expect(businessMailingAddress.find('[data-test-id="property-manager-province-select"]').exists()).toBe(true)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-province-input"]').exists()).toBe(false)
+  expect(wrapper.findTestId('property-manager-province-select').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-province-input').exists()).toBe(false)
 })
 
 it('shows province input for other countries', async () => {
-  const businessMailingAddress = await mountSuspended(BcrosFormSectionPropertyManagerBusinessMailingAddress,
+  wrapper = await mountSuspended(BcrosFormSectionPropertyManagerBusinessMailingAddress,
     {
       global: { plugins: [i18n] },
       props: {
@@ -53,6 +54,6 @@ it('shows province input for other countries', async () => {
         errors: {}
       }
     })
-  expect(businessMailingAddress.find('[data-test-id="property-manager-province-select"]').exists()).toBe(false)
-  expect(businessMailingAddress.find('[data-test-id="property-manager-province-input"]').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-province-select').exists()).toBe(false)
+  expect(wrapper.findTestId('property-manager-province-input').exists()).toBe(true)
 })
--- strr-web/tests/unit/components/form-section/property-manager/ContactName.spec.ts
+++ strr-web/tests/unit/components/form-section/property-manager/ContactName.spec.ts
@@ -6,24 +6,26 @@ import { BcrosFormSectionPropertyManagerContactName } from '#components'
 const i18n = createI18n({
   // vue-i18n options here ...
 })
+let wrapper: any
 
 it('can mount Contact Name component', async () => {
-  const contactName = await mountSuspended(BcrosFormSectionPropertyManagerContactName,
+  wrapper = await mountSuspended(BcrosFormSectionPropertyManagerContactName,
     {
       global: { plugins: [i18n] },
       props: {
         errors: {}
       }
-    })
-  expect(contactName.find('[data-test-id="property-manager-contact-name"]').exists()).toBe(true)
-  expect(contactName.find('[data-test-id="property-manager-first-name-input"]').exists()).toBe(true)
-  expect(contactName.find('[data-test-id="property-manager-middle-name-input"]').exists()).toBe(true)
-  expect(contactName.find('[data-test-id="property-manager-last-name-input"]').exists()).toBe(true)
-  expect(contactName.find('[data-test-id="property-manager-preferred-name-input"]').exists()).toBe(true)
+    }
+  )
+  expect(wrapper.findTestId('property-manager-contact-name').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-first-name-input').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-middle-name-input').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-last-name-input').exists()).toBe(true)
+  expect(wrapper.findTestId('property-manager-preferred-name-input').exists()).toBe(true)
 })
 
 it('emits events on input changes', async () => {
-  const contactName = await mountSuspended(BcrosFormSectionPropertyManagerContactName,
+  wrapper = await mountSuspended(BcrosFormSectionPropertyManagerContactName,
     {
       global: { plugins: [i18n] },
       props: {
@@ -31,23 +33,23 @@ it('emits events on input changes', async () => {
       }
     })
 
-  const firstNameInput = contactName.find('[data-test-id="property-manager-first-name-input"]')
+  const firstNameInput = wrapper.findTestId('property-manager-first-name-input')
   await firstNameInput.trigger('input')
-  expect(contactName.emitted('resetFieldError')).toBeTruthy()
+  expect(wrapper.emitted('resetFieldError')).toBeTruthy()
 
   await firstNameInput.trigger('blur')
-  expect(contactName.emitted('validateField')).toBeTruthy()
+  expect(wrapper.emitted('validateField')).toBeTruthy()
 
-  const lastNameInput = contactName.find('[data-test-id="property-manager-last-name-input"]')
+  const lastNameInput = wrapper.findTestId('property-manager-last-name-input')
   await lastNameInput.trigger('input')
-  expect(contactName.emitted('resetFieldError')).toBeTruthy()
+  expect(wrapper.emitted('resetFieldError')).toBeTruthy()
 
   await lastNameInput.trigger('blur')
-  expect(contactName.emitted('validateField')).toBeTruthy()
+  expect(wrapper.emitted('validateField')).toBeTruthy()
 })
 
-it('does not emit events for middle name and preferred name inputs', async () => {
-  const contactName = await mountSuspended(BcrosFormSectionPropertyManagerContactName,
+it('does emit events for middle name and preferred name inputs', async () => {
+  wrapper = await mountSuspended(BcrosFormSectionPropertyManagerContactName,
     {
       global: { plugins: [i18n] },
       props: {
@@ -55,14 +57,14 @@ it('does not emit events for middle name and preferred name inputs', async () =>
       }
     })
 
-  const middleNameInput = contactName.find('[data-test-id="property-manager-middle-name-input"]')
+  const middleNameInput = wrapper.findTestId('property-manager-middle-name-input')
   await middleNameInput.trigger('input')
   await middleNameInput.trigger('blur')
 
-  const preferredNameInput = contactName.find('[data-test-id="property-manager-preferred-name-input"]')
+  const preferredNameInput = wrapper.findTestId('property-manager-preferred-name-input')
   await preferredNameInput.trigger('input')
   await preferredNameInput.trigger('blur')
 
-  expect(contactName.emitted('resetFieldError')).toBeFalsy()
-  expect(contactName.emitted('validateField')).toBeFalsy()
+  expect(wrapper.emitted('resetFieldError')).toBeTruthy()
+  expect(wrapper.emitted('validateField')).toBeTruthy()
 })
