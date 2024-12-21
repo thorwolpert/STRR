a2f2048470c94d453247d47268f569e32511788e
Dima K <dima1086@hotmail.com>
2024-12-20 16:06:32 -0800

    Updated app config

--- strr-examiner-web/app/app.config.ts
+++ strr-examiner-web/app/app.config.ts
@@ -3,9 +3,34 @@ import { RoutesE } from '~/enums/routes'
 export default defineAppConfig({
   connect: {
     core: {
+      login: {
+        redirectPath: '',
+        idps: () => ['idir']
+      },
       header: {
         options: {
-          localeSelect: false
+          localeSelect: false,
+          unauthenticated: {
+            whatsNew: false,
+            loginMenu: false,
+            createAccount: false
+          },
+          authenticated: {
+            notifications: true,
+            accountOptionsMenu: true
+          }
+        }
+      },
+      plugin: {
+        authApi: {
+          errorRedirect: {
+            401: '/auth/login'
+          }
+        },
+        payApi: {
+          errorRedirect: {
+            401: '/auth/login'
+          }
         }
       }
     }
