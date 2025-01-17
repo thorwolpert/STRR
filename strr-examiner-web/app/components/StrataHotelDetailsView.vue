41c786460fabec5001f8b28d29a2f7a370a49b23
Dima K <dima1086@hotmail.com>
2025-01-17 11:42:21 -0800

    WIP: Strata details page

--- strr-examiner-web/app/components/StrataHotelDetailsView.vue
+++ strr-examiner-web/app/components/StrataHotelDetailsView.vue
@@ -1,71 +1,99 @@
 <script setup lang="ts">
-const props = defineProps<{ application: StrataApplicationResp }>()
 
+const props = defineProps<{application: StrataApplicationResp }>()
+
+const { t } = useI18n()
 const { header, registration } = props.application
+const { applicationNumber } = header
 
 </script>
 
 <template>
   <div>
-    <div
-      class="flex flex-row gap-x-5 divide-x text-sm"
-    >
-      <div>
-        <dl class="grid grid-cols-[repeat(2,auto)] gap-x-4">
-          <dt>Status:</dt>
-          <dd>{{ header?.hostStatus }}</dd>
-
-          <dt>Submitted:</dt>
-          <dd>{{ dateToString(header?.applicationDateTime || '') }}</dd>
-
-          <dt>Registration Type:</dt>
-          <dd>{{ registration?.registrationType }}</dd>
-        </dl>
+    <div class="text-bcGovColor-midGray mb-6 grid grid-cols-4 gap-x-5 divide-x bg-white p-4 text-sm">
+      <div class="space-y-2">
+        <b>{{ t('strr.label.primaryBuilding').toUpperCase() }}</b>
+        <ConnectInfoWithIcon
+          icon="i-mdi-map-marker-outline"
+          :content="displayFullAddress(registration?.strataHotelDetails.buildings[0])"
+        />
       </div>
 
-      <div>
-        <dl class="grid grid-cols-[repeat(2,auto)] gap-x-4 pl-5">
-          <dt>Completing Party</dt>
-          <dd />
-
-          <dt>Name:</dt>
-          <dd>{{ displayContactFullName(registration?.completingParty) }}</dd>
-
-          <dt>Email:</dt>
-          <dd>{{ registration?.completingParty.emailAddress }}</dd>
-
-          <dt>Phone:</dt>
-          <dd>{{ registration?.completingParty.phoneNumber }}</dd>
-        </dl>
-      </div>
-
-      <div>
-        <dl class="grid grid-cols-[repeat(2,auto)] gap-x-4 pl-5">
-          <dt>Strata Hotel Representative</dt>
-          <dd />
-
-          <dt>Name:</dt>
-          <dd>{{ displayContactFullName(registration?.strataHotelRepresentatives[0]) }}</dd>
-
-          <dt>Email:</dt>
-          <dd>{{ registration?.completingParty.emailAddress }}</dd>
-        </dl>
-      </div>
-    </div>
-
-    <!-- APPLICATION SECTIONS -->
-    <div class="mt-6 divide-y">
-      <ApplicationDetailsSection
-        label="Business Details"
-      >
-        <div class="flex items-center justify-between">
-          <div class="flex">
-            {{ registration?.businessDetails?.legalName }} |
-            {{ displayFullAddress(registration?.businessDetails.mailingAddress) }}
-          </div>
+      <div class="space-y-4 pl-5">
+        <div class="space-y-2">
+          <b>{{ t('strr.label.business').toUpperCase() }}</b>
+          <ConnectInfoWithIcon icon="i-mdi-domain" :content="registration?.businessDetails?.legalName" />
+          <ConnectInfoWithIcon
+            icon="i-mdi-envelope-outline"
+            :content="displayFullAddress(registration?.businessDetails.mailingAddress)"
+          />
         </div>
-      </ApplicationDetailsSection>
+        <div class="space-y-2">
+          <b>{{ t('strr.label.attorneyForService').toUpperCase() }}</b>
+          <ConnectInfoWithIcon
+            icon="i-mdi-domain"
+            :content="registration?.businessDetails.registeredOfficeOrAttorneyForServiceDetails.attorneyName"
+          />
+          <ConnectInfoWithIcon
+            icon="i-mdi-map-marker-outline"
+            :content="displayFullAddress(registration?.businessDetails.registeredOfficeOrAttorneyForServiceDetails.mailingAddress)"
+          />
+        </div>
+      </div>
+
+      <div class="space-y-4 pl-5">
+        <div class="space-y-2">
+          <!--Main Representative -->
+          <b>{{ t('strr.label.representative').toUpperCase() }}</b>
+          <ConnectInfoWithIcon
+            icon="i-mdi-account"
+            :content="displayContactFullName(registration?.strataHotelRepresentatives[0] as ApiRep)"
+          />
+
+          <ConnectInfoWithIcon
+            icon="i-mdi-phone"
+            :content="displayPhoneAndExt(registration?.strataHotelRepresentatives[0]?.phoneNumber)"
+          />
+
+          <ConnectInfoWithIcon icon="i-mdi-at" :content="registration?.strataHotelRepresentatives[0]?.emailAddress" />
+        </div>
+        <div v-if="registration?.strataHotelRepresentatives[1]" class="space-y-2">
+          <!-- Second Representative if available -->
+          <b>{{ t('strr.label.secondaryRepresentative').toUpperCase() }}</b>
+          <ConnectInfoWithIcon
+            icon="i-mdi-account"
+            :content="displayContactFullName(registration?.strataHotelRepresentatives[1] as ApiRep)"
+          />
+        </div>
+        <div class="space-y-2">
+          <!-- Completing Party -->
+          <b>{{ t('strr.label.completingParty').toUpperCase() }}</b>
+          <ConnectInfoWithIcon
+            icon="i-mdi-account"
+            :content="displayContactFullName(registration?.completingParty)"
+          />
+        </div>
+      </div>
+
+      <div class="space-y-2 pl-5">
+        <b>{{ t('strr.label.additionalInformation').toUpperCase() }}</b>
+        <div>
+          <b>{{ t('strr.label.numberOfRentalUnits') }}</b>
+          {{ registration.strataHotelDetails.numberOfUnits }}
+        </div>
+      </div>
     </div>
+
+    <ConnectPageSection>
+      <div
+        v-if="props.application.registration.documents?.length > 0"
+        class="divide-y px-10 py-6"
+      >
+        <ApplicationDetailsSection :label="t('strr.label.supportingInfo')">
+          <SupportingDocuments :application="props.application" />
+        </ApplicationDetailsSection>
+      </div>
+    </ConnectPageSection>
   </div>
 </template>
 
