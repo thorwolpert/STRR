8da3665f8ff0fcb68bf26722e21df66c5056b899
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-02-10 16:13:03 -0800

    25480 - migration update
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-api/migrations/versions/20250204_1137_7e9fccbeb3ed_.py
+++ strr-api/migrations/versions/20250204_1137_7e9fccbeb3ed_.py
@@ -17,9 +17,6 @@ depends_on = None
 
 
 def upgrade():
-    stratahotelcategory = postgresql.ENUM('FULL_SERVICE', 'MULTI_UNIT_NON_PR', 'POST_DECEMBER_2023', name='stratahotelcategory')
-    stratahotelcategory.create(op.get_bind(), checkfirst=True)
-
     with op.batch_alter_table('rental_properties', schema=None) as batch_op:
         batch_op.add_column(sa.Column('strata_hotel_category', sa.Enum('FULL_SERVICE', 'MULTI_UNIT_NON_PR', 'POST_DECEMBER_2023', name='stratahotelcategory'), nullable=True))
         batch_op.create_index(batch_op.f('ix_rental_properties_strata_hotel_category'), ['strata_hotel_category'], unique=False)
@@ -37,6 +34,3 @@ def downgrade():
     with op.batch_alter_table('rental_properties_history', schema=None) as batch_op:
         batch_op.drop_index(batch_op.f('ix_rental_properties_history_strata_hotel_category'))
         batch_op.drop_column('strata_hotel_category')
-
-    stratahotelcategory = postgresql.ENUM(name='stratahotelcategory')
-    stratahotelcategory.drop(op.get_bind(), checkfirst=True)    
