c7f4c56f1e53343e1fc1cd831054de9fc04e5c6c
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-02-24 11:50:49 -0800

    25531 fixes
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-host-pm-web/tests/mocks/i18n.ts
+++ strr-host-pm-web/tests/mocks/i18n.ts
@@ -1,5 +1,5 @@
 import { createI18n } from 'vue-i18n'
-import { merge } from 'lodash'
+import merge from 'lodash'
 import baseEn from '../../../strr-base-web/app/locales/en-CA'
 import baseFr from '../../../strr-base-web/app/locales/fr-CA'
 import en from '~~/app/locales/en-CA'
