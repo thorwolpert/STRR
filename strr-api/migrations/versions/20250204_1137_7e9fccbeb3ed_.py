d4d564a74a7467b3e63613ac3495230a6dfffe9b
Shaanjot Gill <gill.shaanjots@gmail.com>
2025-02-24 14:35:20 -0800

    25480 - migration fix
    Signed-off-by: Shaanjot Gill <gill.shaanjots@gmail.com>

--- strr-api/migrations/versions/20250204_1137_7e9fccbeb3ed_.py
+++ strr-api/migrations/versions/20250204_1137_7e9fccbeb3ed_.py
@@ -15,14 +15,16 @@ down_revision = '842bb132abc7'
 branch_labels = None
 depends_on = None
 
+stratahotelcategory = postgresql.ENUM('FULL_SERVICE', 'MULTI_UNIT_NON_PR', 'POST_DECEMBER_2023', name='stratahotelcategory')
+stratahotelcategory.create(op.get_bind(), checkfirst=True)
 
 def upgrade():
     with op.batch_alter_table('rental_properties', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('strata_hotel_category', sa.Enum('FULL_SERVICE', 'MULTI_UNIT_NON_PR', 'POST_DECEMBER_2023', name='stratahotelcategory'), nullable=True))
+        batch_op.add_column(sa.Column('strata_hotel_category', stratahotelcategory, nullable=True))
         batch_op.create_index(batch_op.f('ix_rental_properties_strata_hotel_category'), ['strata_hotel_category'], unique=False)
 
     with op.batch_alter_table('rental_properties_history', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('strata_hotel_category', sa.Enum('FULL_SERVICE', 'MULTI_UNIT_NON_PR', 'POST_DECEMBER_2023', name='stratahotelcategory'), autoincrement=False, nullable=True))
+        batch_op.add_column(sa.Column('strata_hotel_category', stratahotelcategory, autoincrement=False, nullable=True))
         batch_op.create_index(batch_op.f('ix_rental_properties_history_strata_hotel_category'), ['strata_hotel_category'], unique=False)
 
 
