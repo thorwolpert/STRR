2ca77bfa36551d8ab9ab298bd44cb6522026df35
thor <1042854+thorwolpert@users.noreply.github.com>
2026-03-25 16:28:53 -0700

    test(jobs): implement pytest in registration_expiry

--- jobs/registration_expiry/tests/conftest.py
+++ jobs/registration_expiry/tests/conftest.py
@@ -1,13 +1,19 @@
-# pylint: disable=C0114, W0613
-import pytest
+# # pylint: disable=C0114, W0613
+# import pytest
 
-from registration_expiry.job import create_app
+# from registration_expiry.job import create_app
 
 
-@pytest.fixture(scope="session")
-def app(request):
-    """Return a session-wide application configured in TEST mode."""
-    _app = create_app("test")
+# @pytest.fixture(scope="session")
+# def app(request):
+#     """Return a session-wide application configured in TEST mode."""
+#     _app = create_app("test")
 
-    with _app.app_context():
-        yield _app
+#     with _app.app_context():
+#         yield _app
+
+pytest_plugins = [
+    "strr_test_utils.utils_fixtures",
+    "strr_test_utils.db_fixtures",
+    "strr_test_utils.redis_fixtures",
+]
--- jobs/registration_expiry/tests/integration/test_setup.py
+++
@@ -1,5 +0,0 @@
-# pylint: disable=C0114, C0116
-def test_setup(app):
-    print(app.config)
-    print(app.config)
-    print(app.config)
